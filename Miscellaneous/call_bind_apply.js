e3be3d745f5390c6671a110cb2b88970470d94a1
sanny_kumarr <sanny.kumar@zopper.com>
2024-05-26 10:16:45 +0530

    Fibonacci

--- Miscellaneous/call_bind_apply.js
+++ Miscellaneous/call_bind_apply.js
@@ -18,3 +18,16 @@ method.call(obj2, 'Delhi')
 // method.apply(obj2, ['Delhi'])
 const bindData = method.bind(obj2, 'Delhi')
 // bindData()
+
+// ----------------Fibonacci----------------------------------------------------------------------------
+const Fibonacci = (n) => {
+    let n1 = 0, n2 = 1, next = n1 + n2
+    while (next <= n) {
+        // console.log(next)
+        n1 = n2
+        n2 = next
+        next = n1 + n2
+
+    }
+}
+// console.log(Fibonacci(10))
